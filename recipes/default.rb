18a14c9461f7cfe92d2e3f25eb3c415a1aff1685
hieunba <hi3unb@gmail.com>
2018-06-11 01:39:20 +0700

    Installs LLVM using chocolatey by default

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,3 +15,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+if node['llvm']['install_method'] =~ /chocolatey/
+  include_recipe 'llvm::chocolatey'
+else
+  include_recipe 'llvm::setup'
+end
