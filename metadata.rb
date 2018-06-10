3f1291d37866aedaaf308959b20c01395db95de0
hieunba <hi3unb@gmail.com>
2018-06-11 01:38:26 +0700

    Adds recipe to install LLVM using chocolatey

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,8 @@ chef_version '>= 12.6' if respond_to?(:chef_version)
 
 supports 'windows'
 
+depends 'chocolatey', '~> 1.2.1'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
