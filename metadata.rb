6059c07c4c02e850a0c6abc2d1069babcec1973c
hieunba <hi3unb@gmail.com>
2018-06-10 07:38:48 +0700

    Updates metadata

--- metadata.rb
+++ metadata.rb
@@ -5,16 +5,18 @@ license 'Apache-2.0'
 description 'Installs/Configures llvm'
 long_description 'Installs/Configures llvm'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.6' if respond_to?(:chef_version)
+
+supports 'windows'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/llvm/issues'
+issues_url 'https://github.com/hieunba/llvm/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/llvm'
+source_url 'https://github.com/hieunba/llvm'
