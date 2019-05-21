21597441c619b51ce71232bcb057c2b2ee3a12a7
Nghiem Ba Hieu <hi3unb@gmail.com>
2019-05-22 00:53:48 +0700

    Update attributes accordingly

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,7 @@
+package_source = 'http://releases.llvm.org/6.0.0/LLVM-6.0.0-win64.exe'
+package_checksum = '2501887b2f638d3f65b0336f354b96f8108b563522d81e841d5c88c34af283dd'
 
-llvm_6_src_url = 'http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz'
-llvm_6_src_checksum = '1ff53c915b4e761ef400b803f07261ade637b0c269d99569f18040f3dcee4408'
-clang_6_url = 'http://releases.llvm.org/6.0.0/LLVM-6.0.0-win64.exe'
-clang_6_checksum = '2501887b2f638d3f65b0336f354b96f8108b563522d81e841d5c88c34af283dd'
-
-default['llvm']['llvm_src_url'] = llvm_6_src_url
-default['llvm']['llvm_src_checksum'] = llvm_6_src_checksum
-default['llvm']['clang_url'] = clang_6_url
-default['llvm']['clang_checksum'] = clang_6_checksum
+default['llvm']['source'] = package_source
+default['llvm']['checksum'] = package_checksum
 default['llvm']['install_method'] = 'chocolatey'
 default['llvm']['chocolatey']['version'] = '6.0.0.20180508'
