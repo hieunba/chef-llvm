26834301b8c449f9862d29fb5fd2beab857fc08c
Nghiem Ba Hieu <hi3unb@gmail.com>
2019-05-22 00:51:27 +0700

    add test fixture cookbook for testing purpose

---
+++ test/cookbooks/test/metadata.rb
@@ -0,0 +1,12 @@
+name 'test'
+maintainer 'Nghiem Ba Hieu'
+maintainer_email 'hi3unb@gmail.com'
+license 'Apache-2.0'
+description 'Tests llvm cookbook'
+long_description 'Tests llvm cookbook'
+version '0.1.0'
+chef_version '>= 13.5.3'
+
+supports 'windows'
+
+depends 'llvm'
