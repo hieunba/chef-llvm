e2056102f227dfe8e39530c9dc05ee94be2ef5a6
Nghiem Ba Hieu <hi3unb@gmail.com>
2019-05-22 14:51:43 +0700

    Updates source and issues urls

--- metadata.rb
+++ metadata.rb
@@ -16,10 +16,10 @@ depends 'windows'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-issues_url 'https://github.com/hieunba/llvm/issues'
+issues_url 'https://github.com/hieunba/chef-llvm/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-source_url 'https://github.com/hieunba/llvm'
+source_url 'https://github.com/hieunba/chef-llvm'
